fcf072c8706fc427a8d880ce6bb55bb65984432a
Ratludu <119836270+Ratludu@users.noreply.github.com>
2025-08-27 20:31:39 +1000

    added json.go

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/Ratludu/Gopher-It/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
+)
+
+type apiConfig struct {
+	DB *database.Queries
+}
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Could not read in .env file %v", err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set)")
+	}
+
+	apiCfg := apiConfig{}
+
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Printf("DATABASE_URL environment variable not set")
+	} else {
+		db, err := sql.Open("libsql", dbURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		dbQueries := database.New(db)
+		apiCfg.DB = dbQueries
+		log.Println("Connected to database!")
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("listening and serving files on port: %s", port)
+	log.Fatal(srv.ListenAndServe())
+}
---
+++ sql/queries/users.sql
@@ -0,0 +1,15 @@
+-- name: CreateUser :one
+INSERT INTO users (id, created_at, updated_at, name, api_key)
+VALUES (
+    ?,
+    ?,
+    ?,
+    ?,
+    ?
+)
+RETURNING *;
+--
+
+-- name: GetUser :one
+SELECT * FROM users WHERE api_key = ?;
+--
